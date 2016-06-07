e09afef51177ddf53b1cca484cedc1cb07533584
Justin Neil <justinmneil@gmail.com>
2016-06-07 14:35:49 -0400

    REAL initial commit. Created Physics/Rocketry.ex file to calculate escape velocity.

---
+++ physics/lib/physics/rocketry.ex
@@ -0,0 +1,15 @@
+defmodule Physics.Planet do
+	defstruct [
+		name: "Earth",
+		radius_m: 6.371e6,
+		mass_kg: 5.97e24
+	]
+end
+
+
+
+defmodule Physics.Rocketry do
+	def escape_velocity do
+		
+	end
+end
