deaed08172f70c7fb5ecf29de565c6dacacbc3d1
Justin Neil <justinmneil@gmail.com>
2016-06-07 19:05:06 -0400

    Modified rocketry and physics_test

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -23,8 +23,18 @@ defmodule Physics.Planet do
 	defstruct [
 		name: "Earth",
 		radius_m: 6.371e6,
-		mass_kg: 5.97e24
-	]
+		mass_kg: 5.972e24
+	]	
+
+end
+
+
+defmodule Physics.Rocketry do
+	
+	def escape_velocity(:earth) do
+		%{mass: 5.972e24, radius: 6.371e6}
+		|> escape_velocity
+	end
 
 	def escape_velocity(planet) when is_map(planet) do
 		planet
@@ -35,17 +45,8 @@ defmodule Physics.Planet do
 
 	defp calculate_escape(%{mass: mass, radius: radius}) do
 		newtons_constant = 6.67e-11
-		2 * g * planet.mass_kg/planet.radius_m
+		2 * newtons_constant * mass/radius
 		|> :math.sqrt
 	end
 
-	
-
-	
-end
-
-v = %Physics.Planet{} |> Physics.Planet.escape_velocity
-
-defmodule Physics.Rocketry do
-	
 end
