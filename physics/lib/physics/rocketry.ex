c57d3ea71d6dabf34fbd82e4b5f2b6b0b9e6f536
Justin Neil <justinmneil@gmail.com>
2016-06-08 10:30:46 -0400

    Added logic and tests for mars and moon escape velocity

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -32,9 +32,26 @@ end
 defmodule Physics.Rocketry do
 	
 	def escape_velocity(:earth) do
-		%{mass: 5.972e24, radius: 6.371e6}
+		%{mass: 5.972e24, radius_m: 6.371e6}
 		|> escape_velocity
 	end
+
+	def escape_velocity(:mars) do
+		%{mass: 6.39e23, radius_m: 3.389278e6}
+		|> escape_velocity
+	end
+
+	def escape_velocity(:moon) do
+		%{mass: 7.35e22, radius_m: 1737.5}
+		|> escape_velocity
+	end
+
+	def escape_velocity(:moon) do
+		%{mass: 7.35e22, radius_m: 1737.5}
+		|> escape_velocity
+	end
+
+	
 
 	def escape_velocity(planet) when is_map(planet) do
 		planet
@@ -43,9 +60,9 @@ defmodule Physics.Rocketry do
 		|> Converter.to_nearest_tenth		
 	end
 
-	defp calculate_escape(%{mass: mass, radius: radius}) do
+	defp calculate_escape(%{mass: mass, radius_m: radius_m}) do
 		newtons_constant = 6.67e-11
-		2 * newtons_constant * mass/radius
+		2 * newtons_constant * mass/radius_m
 		|> :math.sqrt
 	end
 
--- physics/test/physics_test.exs
+++ physics/test/physics_test.exs
@@ -2,14 +2,28 @@ defmodule PhysicsTest do
   use ExUnit.Case
   doctest Physics
 
-  test "escape velocity of earth is correct" do
+  test "escape velocity of earth is correct: " do
   	ev = Physics.Rocketry.escape_velocity(:earth)
+    |> IO.inspect
     assert ev == 11.2
   end
 
-  test "escape velocity of planet x is correct" do
+  test "escape velocity of mars is correct: " do
+  	ev = Physics.Rocketry.escape_velocity(:mars)
+    |> IO.inspect
+    assert ev == 11.2
+  end
+
+  test "escape velocity of the moon is correct: " do
+  	ev = Physics.Rocketry.escape_velocity(:moon)
+    |> IO.inspect
+    assert ev == 11.2
+  end
+
+  test "escape velocity of planet x is correct: " do
     ev = %{mass: 4.0e22, radius: 6.21e6}
     |> Physics.Rocketry.escape_velocity
+    |> IO.inspect
     assert ev == 1.0
   end
 
